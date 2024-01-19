35fc33e4bedbbedeaa7a3f4f5a95301caee48996
Mohamed HAMMANE <mohamed-hammane@outlook.fr>
2024-01-19 10:28:33 +0100

    Images view

--- kImages/Controllers/HomeController.cs
+++ kImages/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 
 namespace kImages.Controllers
 {
     public class HomeController : Controller
     {
         public string Index() => "kImages";
+        public ActionResult Images()
+        {
+            var files = new System.IO.DirectoryInfo(Helpers.ImagesPath()).GetFiles();
+            var destPath = new System.Web.UI.Page().Server.MapPath("~\\Content");
+            foreach (var f in files) f.CopyTo($"{destPath}/{f.Name}", true);
+            return View("Images", files.Select(f => f.Name).Where(name => name.ToLower().EndsWith("png") || name.ToLower().EndsWith("jpeg") || name.ToLower().EndsWith("jpg")).ToArray());
+        }
     }
 }
